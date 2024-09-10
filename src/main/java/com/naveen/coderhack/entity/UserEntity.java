d1fedbad47bb628882b2a0a2de8c6afe4c90ecef
Naveen Kumar <naveenkumarerroju@gmail.com>
2024-09-10 17:05:02 +0530

    rework on entity for reducing boilor-plate using lambok.

--- src/main/java/com/naveen/coderhack/entity/UserEntity.java
+++ src/main/java/com/naveen/coderhack/entity/UserEntity.java
@@ -1,6 +1,10 @@
 package com.naveen.coderhack.entity;
 
 import jakarta.validation.constraints.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.Field;
@@ -9,6 +13,10 @@ import java.util.List;
 import java.util.Objects;
 
 @Document(collection = "users") // Maps the class to the MongoDB "users" collection
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class UserEntity {
 
     @Id
@@ -28,63 +36,6 @@ public class UserEntity {
     @Size(min = 1, max = 3, message = "Array must contain between 1 and 3 elements")
     @Field("badges")
     private List<String> badges;  // List of user badges (1 to 3 elements)
-
-    // Constructors
-    public UserEntity() {
-    }
-
-    public UserEntity(String userId, String username, int score, List<String> badges) {
-        this.userId = userId;
-        this.username = username;
-        this.score = score;
-        this.badges = badges;
-    }
-
-    // Getters and Setters
-
-    public String getUserId() {
-        return userId;
-    }
-
-    public void setUserId(String userId) {
-        this.userId = userId;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public int getScore() {
-        return score;
-    }
-
-    public void setScore(int score) {
-        this.score = score;
-    }
-
-    public List<String> getBadges() {
-        return badges;
-    }
-
-    public void setBadges(List<String> badges) {
-        this.badges = badges;
-    }
-
-    // toString method
-
-    @Override
-    public String toString() {
-        return "UserEntity{" +
-                "userId='" + userId + '\'' +
-                ", username='" + username + '\'' +
-                ", score=" + score +
-                ", badges=" + badges +
-                '}';
-    }
 
     // equals and hashCode methods
 
